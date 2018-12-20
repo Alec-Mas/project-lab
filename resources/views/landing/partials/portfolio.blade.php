dc4035d07f26c7b1c484b21c126b8180651ad524
Alec <alecmaslij@gmail.com>
2018-12-21 10:28:48 +1100

    Added support for viewing uploaded images

--- resources/views/landing/partials/portfolio.blade.php
+++ resources/views/landing/partials/portfolio.blade.php
@@ -3,7 +3,7 @@
     @foreach($posts as $p)
     <div class="col-lg-3 col-md-6 mb-4">
         <div class="card">
-            <img class="card-img-top" src="http://placehold.it/500x325" alt="">
+            <img class="card-img-top" src="{{asset('storage/'.$p->post_thumbnail)}}" alt="">
             <div class="card-body">
             <h4 class="card-title">{{ $p->post_title }}</h4>
             <p class="card-text">{{ $p->post_brief }}</p>
