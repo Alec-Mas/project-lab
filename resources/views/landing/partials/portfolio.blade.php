f29d936439b88a25f4bc706f6cbddc6dd2963096
Alec <alecmaslij@gmail.com>
2018-12-21 11:44:20 +1100

    Fixed a styling issue.

--- resources/views/landing/partials/portfolio.blade.php
+++ resources/views/landing/partials/portfolio.blade.php
@@ -3,7 +3,7 @@
     @foreach($posts as $p)
     <div class="col-lg-3 col-md-6 mb-4">
         <div class="card">
-            <img class="card-img-top" src="{{asset('storage/'.$p->post_thumbnail)}}" alt="">
+            <img class="card-img-top" style="height:200px; max-height:200px;" src="{{asset('storage/'.$p->post_thumbnail)}}" alt="">
             <div class="card-body">
             <h4 class="card-title">{{ $p->post_title }}</h4>
             <p class="card-text">{{ $p->post_brief }}</p>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,16 @@
 </head>
 <body>
     @extends('navigation.navigation')
-
+    @if (count($errors) > 0)
+    <div class="alert alert-danger">
+        <strong>Whoops!</strong> There were some problems with your input.<br><br> 
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
     @yield('content')
 
     {{-- Scripts --}}
