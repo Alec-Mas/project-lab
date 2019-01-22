149016ae77d0e9ff0093d27448c6e1352e56d307
Alec <alecmaslij@gmail.com>
2019-01-23 09:51:59 +1100

    Fixed the spacing issue between a posts content and the document footer.

--- resources/views/portfolio/index.blade.php
+++ resources/views/portfolio/index.blade.php
@@ -14,35 +14,35 @@
         </div>
     </div>
     @if(Auth::check())
-    <!-- Documentation -->
-    <div class="table table-responsive mt-4">
+        <!-- Documentation -->
         @if(count($post->documents) > 0)
-        <table class="table">
-            <thead>
-                <tr>
-                <th scope="col">Document Name</th>
-                <th scope="col">Document Created</th>
-                <th scope="col">Document Last Updated</th>
-                </tr>
-            </thead>
-            @foreach($post->documents as $d)
-            <tbody>
-                <tr>
-                    <td>{{ $d->document_name }}</td>
-                    <td>{{ $d->created_at->diffForHumans() }}</td>
-                    <td>{{ $d->updated_at->diffForHumans() }}</td>
-                    <td>
-                        <a class="btn btn-success mb-1" href="{{ $d->document_link }}">
-                            <i class="fa fa-download icon-large"></i>
-                        </a>
-                        <button href="#" data-toggle="modal" data-id="{{ $post->id }}"data-document="{{ $d->id }}" data-target="#editDocument" class="btn btn-primary mb-1"><i class="fa fa-wrench icon-large"></i></button>
-                    </td>
-                </tr>
-            </tbody>
-            @endforeach
-        </table>
+        <div class="table table-responsive mt-4">
+            <table class="table">
+                <thead>
+                    <tr>
+                    <th scope="col">Document Name</th>
+                    <th scope="col">Document Created</th>
+                    <th scope="col">Document Last Updated</th>
+                    </tr>
+                </thead>
+                @foreach($post->documents as $d)
+                <tbody>
+                    <tr>
+                        <td>{{ $d->document_name }}</td>
+                        <td>{{ $d->created_at->diffForHumans() }}</td>
+                        <td>{{ $d->updated_at->diffForHumans() }}</td>
+                        <td>
+                            <a class="btn btn-success mb-1" href="{{ $d->document_link }}">
+                                <i class="fa fa-download icon-large"></i>
+                            </a>
+                            <button href="#" data-toggle="modal" data-id="{{ $post->id }}"data-document="{{ $d->id }}" data-target="#editDocument" class="btn btn-primary mb-1"><i class="fa fa-wrench icon-large"></i></button>
+                        </td>
+                    </tr>
+                </tbody>
+                @endforeach
+            </table>
+        </div>
         @endif
-    </div>
     @endif
     @if(Auth::check())
     <div class="card mt-4">
